f2e40a887f3661c9ee07dd3f64434256700461e9
chadandsteff <chad@chadandstephanieweirick.com>
2018-08-11 13:15:21 -0700

    not sure
    GIT sucks

---
+++ src/Comparable.java
@@ -0,0 +1,20 @@
+/**
+ * Grand Canyon University 
+ * CST-135
+ * Brandon Bass
+ * 
+ * Daniel"Thad"Dittmer
+ * Chad Wierick
+ * David Pratt
+ * Zachary Swoveland
+ * 
+ * This is our candy class which extends snacks which in turn extends product
+ */
+
+public interface Comparable {
+	
+	boolean compareTo(Product b);
+	
+	
+	
+}
